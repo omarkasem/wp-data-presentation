1f06a1d20a6be8f330237647d2799e04c7f3d643
omarkasem <omar.kasem207@gmail.com>
2023-12-18 08:31:45 +0200

    fix

--- includes/class-wpdp-metabox.php
+++ includes/class-wpdp-metabox.php
@@ -154,11 +154,13 @@ final class WPDP_Metabox {
     }
 
     public function shortcode_box($field){
-        echo '<div class="wpdp_shortcode">
-        <input type="text" disabled value=" [WP_DATA_PRESENTATION id='.$_GET['post'].']"> 
-        <button class="button button-secondary wpdp_copy">Copy</button>
-        <input type="hidden" class="wpdp_validated" name="wpdp_validated" value="0">
-        </div>';
+        if(isset($_GET['post'])){
+            echo '<div class="wpdp_shortcode">
+            <input type="text" disabled value=" [WP_DATA_PRESENTATION id='.$_GET['post'].']"> 
+            <button class="button button-secondary wpdp_copy">Copy</button>
+            <input type="hidden" class="wpdp_validated" name="wpdp_validated" value="0">
+            </div>';
+        }
     }
     
     public function get_data(){
